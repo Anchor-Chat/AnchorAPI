2053e800fa54927640701c798800095edec20fa0
lukas2005 <lukas2005.38@gmail.com>
2018-12-02 21:53:15 +0100

    Fixed the polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,5 +1,6 @@
 export function eventEmitter(ee) {
     ee.prependListener = function (type, listener) {
+        if (!this._events[type]) this._events[type] = [];
         this._events[type].unshift(listener);
         return this;
     };

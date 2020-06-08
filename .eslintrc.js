26fccf64940f14880ef081c719e1ba6d3b5b2503
Łukasz Mariański <lmarianski@protonmail.com>
2020-06-08 21:05:50 +0200

    Changed name to @anchor-chat/api

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,8 +14,8 @@ module.exports = {
 		'ecmaVersion': 11
 	},
 	'rules': {
-		'no-console': process.env.NODE_ENV  === 'production' ? ['error'] : ['off'],
-		'no-debugger': process.env.NODE_ENV === 'production' ? ['error'] : ['off'],
+		'no-console': process.env.NODE_ENV  === 'x' ? ['warn'] : ['off'],
+		'no-debugger': process.env.NODE_ENV === 'x' ? ['warn'] : ['off'],
 		'indent': ['error','tab'],
 		'linebreak-style': ['error', 'unix'],
 		'quotes': ['error','single'],
